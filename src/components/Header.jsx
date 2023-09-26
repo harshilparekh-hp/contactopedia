ad603c2d2b233cd5a12498f43044963c4ce5a353
hparekh <mail.hparekh@gmail.com>
2023-09-26 16:59:47 -0500

    Cleaning up unnecessary files and structure created

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "../images/logo192.png";
+
+const Header = () => {
+  return (
+    <div className="pt-3 py-2 pl-2" style={{ backgroundColor: "black" }}>
+      <img src={logo} style={{ height: "35px", verticalAlign: "top" }}></img>{" "}
+      &nbsp;
+      <span className="h2 pt-4 text-white-50">Contact-O-Pedia</span>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/MainBody.jsx
@@ -0,0 +1,13 @@
+import react from 'react';
+
+const MainBody = () => {
+    return(
+        <div className='row'>
+            <div className='col'>
+                Lets start
+            </div>
+        </div>
+    )
+}
+
+export default MainBody;
