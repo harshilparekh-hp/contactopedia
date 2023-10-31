6a354197b222542553fac9f5baa28251f22fd6d2
hparekh <mail.hparekh@gmail.com>
2023-10-30 22:17:02 -0500

    Header Updated

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className="pt-3 py-2 pl-2" style={{ backgroundColor: "black" }}>
       <img src={logo} style={{ height: "35px", verticalAlign: "top" }}></img>{" "}
       &nbsp;
-      <span className="h2 pt-4 text-white-50">Contact-O-Pedia</span>
+      <span className="h2 pt-4 text-white-50">Contact-O-Pedia | Static React CRUD App |  </span>
     </div>
   );
 };
